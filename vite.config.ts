55ac5cbff030167850f9a78456e13f9d7f72a207
Angriff36 <unashamed366@gmail.com>
2025-09-04 05:57:59 -0700

    Fix Demo Environment Variable Access

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'demo',
   define: {
     'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL || ''),
     'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || ''),
