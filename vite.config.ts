cb7bb49add4daa91767ad37edb2926847906b4cf
Angriff36 <unashamed366@gmail.com>
2025-09-04 06:59:11 -0700

    Add Vite Configuration to Handle JSX Syntax

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   root: 'demo', 
   envDir: '../', // Look for .env in the parent directory
   // Environment variables starting with VITE_ are automatically available
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    loader: 'tsx'
+  },
   build: {
     target: 'es2022' // Support top-level await
   },
